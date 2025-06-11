6fe919d2a9d4d36cadaa9fed50fdbc81f0b3b67d
Nivesh Pritmani <niveshpritmani@gmail.com>
2025-06-11 15:31:21 +0530

    upgraded to motor

--- database.py
+++ database.py
@@ -1,11 +1,11 @@
-from pymongo import AsyncMongoClient
 from config import Config
+from motor.motor_asyncio import AsyncIOMotorClient
 
 
 def get_session():
-     
+
     try:
-        return AsyncMongoClient(Config.MONGO_CONN_STR)
-        
+        return AsyncIOMotorClient(Config.MONGO_CONN_STR)
+
     except Exception as e:
         raise Exception(e)
