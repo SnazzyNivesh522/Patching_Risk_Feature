0acf53ea1ed6363cc6dc7f60d8e6fc9efe8d772b
Nivesh Pritmani <niveshpritmani@gmail.com>
2025-06-12 16:30:10 +0530

    commented old logic to download cves

--- main.py
+++ main.py
@@ -7,18 +7,18 @@ if __name__ == "__main__":
     asyncio.run(load_all_metadata())
     print(f"Total Time Taken: {time.time() - start_time:.0f} seconds")
 
-    start_time = time.time()
-    zip_filename = download_cve_list()
-    print(
-        f"Total Time Taken to download CVE list: {time.time() - start_time:.0f} seconds"
-    )
-    print(f"Downloaded CVE list zip file: {zip_filename}")
+    # start_time = time.time()
+    # zip_filename = download_cve_list()
+    # print(
+    #     f"Total Time Taken to download CVE list: {time.time() - start_time:.0f} seconds"
+    # )
+    # print(f"Downloaded CVE list zip file: {zip_filename}")
 
-    if zip_filename:
-        start_time = time.time()
-        extract_nested_zip(zip_filename, extract_to_dir="cve_data")
-        print(
-            f"Total Time Taken to extract CVE list: {time.time() - start_time:.0f} seconds"
-        )
-    else:
-        print("⚠️ CVE list download failed. Extraction skipped.")
+    # if zip_filename:
+    #     start_time = time.time()
+    #     extract_nested_zip(zip_filename, extract_to_dir="cve_data")
+    #     print(
+    #         f"Total Time Taken to extract CVE list: {time.time() - start_time:.0f} seconds"
+    #     )
+    # else:
+    #     print("⚠️ CVE list download failed. Extraction skipped.")
