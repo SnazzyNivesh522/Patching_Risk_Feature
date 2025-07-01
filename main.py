a127f5828e48421f0859c3497ab4b6e858770267
Nivesh Pritmani <niveshpritmani@gmail.com>
2025-07-01 19:55:30 +0530

    minor changes

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from script import load_all_metadata, download_cve_list, extract_nested_zip
+from script_nvd_cve import main as nvc_cve_main
 import asyncio
 import time
 
 if __name__ == "__main__":
     start_time = time.time()
     asyncio.run(load_all_metadata())
-    print(f"Total Time Taken: {time.time() - start_time:.0f} seconds")
+    print(f"Total Time Taken for metadata: {time.time() - start_time:.0f} seconds")
 
+    start_time=time.time()
+    asyncio.run(nvc_cve_main())
+    print(f"Total Time Taken for NVD CVE data: {time.time() - start_time:.0f} seconds")
     # start_time = time.time()
     # zip_filename = download_cve_list()
     # print(
